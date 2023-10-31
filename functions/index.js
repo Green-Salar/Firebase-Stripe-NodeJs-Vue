85e3898b5c3b24b5f609720b14eba4004f6b4634
Salar <salar.rezayani@gmail.com>
2023-10-31 11:20:44 -0400

    start html no function

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-//const {onRequest} = require("firebase-functions/v2/https");
-//const logger = require("firebase-functions/logger");
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -17,18 +17,20 @@
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
-const functions = require('firebase-functions');
-const express = require('express');
-const path = require('path');
+const functions = require("firebase-functions");
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
-app.get('/second', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'second.html'));
+app.get("/second", (req, res) => {
+    const filePath = path.join(__dirname, "../public", "second.html");
+    console.log("Serving file from:", filePath);
+    res.sendFile(filePath);
 });
 
 exports.app = functions.https.onRequest(app);
